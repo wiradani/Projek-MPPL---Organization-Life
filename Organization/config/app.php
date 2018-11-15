79069dc3f3d6690cb818ad45801b1cfa33d514c7
Saeful Ramadhan <sflramadhan@gmail.com>
2018-11-15 08:35:39 +0700

    first push

--- Organization/config/app.php
+++ Organization/config/app.php
@@ -137,6 +137,7 @@ return [
         /*
          * Laravel Framework Service Providers...
          */
+        JeroenNoten\LaravelAdminLte\ServiceProvider::class,
         Illuminate\Auth\AuthServiceProvider::class,
         Illuminate\Broadcasting\BroadcastServiceProvider::class,
         Illuminate\Bus\BusServiceProvider::class,
