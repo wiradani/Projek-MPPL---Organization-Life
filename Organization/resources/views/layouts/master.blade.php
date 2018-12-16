ee70c2d18f3114df7ec1b233889400bcc438baca
pranaathome <pranayadnaap96@gmail.com>
2018-12-16 21:27:58 +0700

    edit sidebar

--- Organization/resources/views/layouts/master.blade.php
+++ Organization/resources/views/layouts/master.blade.php
@@ -143,7 +143,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
+    <a href="{{ url('/') }}" class="brand-link">
       <img src="../images/OL_logo.svg" alt="OrganizationLIFE Logo" class="brand-image img-circle elevation-3"
            style="opacity: .8">
       <span class="brand-text font-weight-light">OrganizationLIFE</span>
@@ -151,22 +151,37 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
     <!-- Sidebar -->
     <div class="sidebar">
-      {{-- <!-- Sidebar user panel (optional) -->
+      <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+          <img src="../images/profile.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
           <a href="#" class="d-block">Alexander Pierce</a>
         </div>
-      </div> --}}
+      </div>
 
       <!-- Sidebar Menu -->
       <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
-          <li class="nav-item has-treeview menu-open">
+          
+            <li class="nav-item">
+            <a href="/tambahEvent" class="nav-link">
+              <i class="fa fa-circle-o nav-icon"></i>
+              <p>Tambah Event</p>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a href="/tabelEvent" class="nav-link">
+              <i class="fa fa-circle-o nav-icon"></i>
+              <p>Tabel Event</p>
+            </a>
+          </li>
+
+          {{-- <li class="nav-item has-treeview menu-open">
             <a href="#" class="nav-link active">
               <i class="nav-icon fa fa-dashboard"></i>
               <p>
@@ -197,7 +212,8 @@ scratch. This page gets rid of all links and provides the needed markup only.
                 <span class="right badge badge-danger">New</span>
               </p>
             </a>
-          </li>
+          </li> --}}
+
         </ul>
       </nav>
       <!-- /.sidebar-menu -->
