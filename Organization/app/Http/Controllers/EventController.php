7b8d59aee654f0c83f67994a71135a7b60bb4e4a
wiradani <adhan432@gmail.com>
2018-12-17 21:29:01 +0700

    allow cors + new api userEventHistory

--- Organization/app/Http/Controllers/EventController.php
+++ Organization/app/Http/Controllers/EventController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Event;
 
 class EventController extends Controller
@@ -36,5 +37,15 @@ class EventController extends Controller
         $event->delete();
 
         return 204;
+    }
+
+    public function userEventHistory(Request $request, $id){
+        $user = DB::table('users')
+            ->join('user_event', 'users.id', '=', 'user_event.user_id')
+            ->join('events', 'user_event.event_id', '=', 'events.id_event')
+            ->select('users.id','events.id_event','events.nama_event','events.deskripsi_event')
+            ->where('users.id', '=', $id)
+            ->get();
+        return response()->json($user, 200);
     }
 }
